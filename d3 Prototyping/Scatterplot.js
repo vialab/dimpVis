a269b506b020f5b4ae4eb645738bdcd6b47168c8
Brittany Kondo <brittany.kondo@gmail.com>
2013-01-17 16:45:43 -0500

    Drag one point along a path without having mouse directly on path

--- d3 Prototyping/Scatterplot.js
+++ d3 Prototyping/Scatterplot.js
@@ -17,7 +17,7 @@ function Scatterplot(x, y, w, h, id) {
    this.displayData = [];
    this.dataLength = 0;
    
-   this.clickedPoint = null;
+   this.clickedPoint = -1;
    this.hoveredPoint = -1;
    this.dragEvent = null;
    // Function objects, these should be implemented by the controller
@@ -50,7 +50,7 @@ Scatterplot.prototype.init = function() {
       .attr("height", this.height)
       .style("position", "absolute")
       .style("left", this.xpos + "px")
-      .style("top", this.ypos + "px")      
+      .style("top", this.ypos + "px")        	  
    ; 
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -88,23 +88,24 @@ Scatterplot.prototype.render = function( vdata ) {
 	  .style("cursor", "pointer")     
    ;  
    
- //Testing drawing paths between two points
- var path1 = [{x:100, y:33},{x:300,y:95}];
+ //Testing drawing paths between points
+ /**var path1data = [{x:100, y:33},{x:300,y:95},{x:400,y:90}];
  var line = d3.svg.line()
     .x(function(d) { return d.x; })
     .y(function(d) { return d.y; })
     .interpolate("basis");	
  
  this.widget.selectAll("g").append("svg:path")
-                                  .attr("d", line(path1))
+                                  .attr("d", line(path1data))
 								  .style("stroke-width", 2)
 								  .style("stroke", "steelblue")
-								   .style("fill", "none");  
+								   .style("fill", "none");  */
    
   
 }
-//Updates the display when a drag event is occurring
-Scatterplot.prototype.updateDrag = function() {
+//TODO:make sure doesn't go out of bounds!
+//Updates the point location when a point is dragged
+Scatterplot.prototype.updateDraggedPoint = function() {
     this.widget.selectAll("circle")
 	 .attr("cx", function(d) {
         return d.x;
@@ -114,7 +115,54 @@ Scatterplot.prototype.updateDrag = function() {
       });	
   
 }
-
+//Updates the display when a point is being dragged 
+Scatterplot.prototype.updateDrag = function(clickedPoint) {
+    
+    
+	 //Code adapted from: http://bl.ocks.org/3824661
+	 var path1data = [{x:100, y:33},{x:300,y:95},{x:400,y:90}];
+     var line = d3.svg.line()
+    .x(function(d) { return d.x; })
+    .y(function(d) { return d.y; })
+    .interpolate("basis");	
+ 
+    var path = this.widget.append("svg:path")
+                                  .attr("d", line(path1data))
+								  .style("stroke-width", 2)
+								  .style("stroke", "steelblue")
+								   .style("fill", "none");
+	 var pathEl = path.node();
+	 var pathLength = pathEl.getTotalLength();
+	 var BBox = pathEl.getBBox(); //gets the bounding box of the geometry
+	 var scale = pathLength/BBox.width;
+	 var offsetLeft = document.getElementById("scatter").offsetLeft;
+	 
+	 var x = d3.event.pageX - offsetLeft; 
+	 console.log(x);
+      var beginning = x, end = pathLength, target;
+      while (true) {
+        target = Math.floor((beginning + end) / 2);
+        pos = pathEl.getPointAtLength(target);
+        if ((target === end || target === beginning) && pos.x !== x) {
+            break;
+        }
+        if (pos.x > x)      end = target;
+        else if (pos.x < x) beginning = target;
+        else                break; //position found
+      }
+	  this.widget.selectAll("circle")     
+        .attr("cx", function(d){
+		      if (d.id == clickedPoint)
+			      return x;
+			  return d.x;
+		})
+        .attr("cy", function(d){
+		     if(d.id==clickedPoint)
+			     return pos.y;
+			return d.y;
+		});
+  
+}
 
 
 
--- d3 Prototyping/testD3.js
+++ d3 Prototyping/testD3.js
@@ -20,30 +20,38 @@ scatterplot.init();
 ////////////////////////////////////////////////////////////////////////////////
 // Define some interaction functions for the scatterplot
 ////////////////////////////////////////////////////////////////////////////////
- scatterplot.dragEvent = d3.behavior.drag()
+ /**scatterplot.dragEvent = d3.behavior.drag()
                        .origin(function(d){ //Set the starting point of the drag interaction
 							return d;
 	                   })
                       .on("drag", function(d){
 					       d.x = d3.event.x;
-						   d.y = d3.event.y;                            			   
-                           scatterplot.updateDrag();						   
-					  });	
+						   d.y = d3.event.y; 						 
+                           scatterplot.updateDraggedPoint();						   
+					  });	*/
 //Mouse in, out and click
 scatterplot.widget.selectAll("circle")
 				  .on("click", function (d){ 
-                        scatterplot.clickedPoint = d.id;                        						
-						console.log(scatterplot.clickedPoint);
+				        if (scatterplot.clickedPoint != d.id)
+                             scatterplot.clickedPoint = d.id;
+                        else 
+							scatterplot.clickedPoint = -1;
+						console.log("clicked"+scatterplot.clickedPoint);
 	              })
 	              .on("mouseover", function (d){
 					    scatterplot.hoveredPoint = d.id;
-						console.log(scatterplot.hoveredPoint);
+						console.log("hovered"+scatterplot.hoveredPoint);
 	              })
 				  .on("mouseout", function (d){
 						console.log("cleared");
-	               })
-                   .call(scatterplot.dragEvent);
+	               })                  			  
+                   /**.call(scatterplot.dragEvent)*/;
 
 
 
+scatterplot.widget.on("mousemove", function (d){
+		  //console.log(d3.svg.mouse(this)); //older version of d3, need to use d3.svg.mouse
+		  if (scatterplot.clickedPoint != -1)
+			scatterplot.updateDrag(scatterplot.clickedPoint);
 
+	   });
